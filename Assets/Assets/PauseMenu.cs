120c3fe77c22356e9132e4825d99189e3e4ed539
JUwUles <jules.schmidt.1erdunom@gmail.com>
2025-06-03 00:01:30 +0200

    pause menu

--- Assets/Assets/PauseMenu.cs
+++ Assets/Assets/PauseMenu.cs
@@ -7,32 +7,36 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseMenuUI;
     private bool isPaused = false;
 
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
-                Resume();
+                resume();
             else
-                Pause();
+                pause();
         }
     }
 
-    public void Resume()
+    public void resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
 
-    public void Pause()
+    public void pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
-    public void LoadMainMenu()
+    public void loadMainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu"); // mettre nom de la scene
